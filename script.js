0344cff530e2bd0ef72d72cbee7dd4d419ab0045
CharlieGroves <62213643+CharlieGroves@users.noreply.github.com>
2020-07-12 13:10:42 +0100

    fireworks

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
-  }
+}
   
   function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
-  }
+}
+
+function pauseFireworks() {
+  var a = document.getElementById('1');
+  var b = document.getElementById('2');
+  var c = document.getElementById('3');
+  a.classList.add('paused');
+  b.classList.add('paused');
+  c.classList.add('paused');
+}
+
+function resumeFireworks() {
+  var a = document.getElementById('1');
+  var b = document.getElementById('2');
+  var c = document.getElementById('3');
+  a.classList.remove('paused');
+  b.classList.remove('paused');
+  c.classList.remove('paused');
+}
